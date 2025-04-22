7623ced77532be6ed6b0d693739bd4036f33ed89
tiago-velosa <ttvelosa2008@gmail.com>
2025-04-22 05:12:34 -0700

    Update Exercicio4.c

--- UFCD10789/Exercicio4/Exercicio4.c
+++ UFCD10789/Exercicio4/Exercicio4.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+    float saldo_inicial, cheque;
+    printf("Insira o saldo inicial do cliente:  ");
+    scanf("%f", &saldo_inicial);
+    printf("Insira o valor do cheque:  ");
+    scanf("%f", &cheque);
+    if (saldo_inicial >= cheque) {
+        saldo_inicial = saldo_inicial - cheque;  
+        printf("Cheque aprovado! O saldo após o desconto será € %.2f\n", saldo_inicial);
+    } else {
+        printf("Cheque recusado! Saldo insuficiente.\n");
+    }
+    return 0;
+}
 
