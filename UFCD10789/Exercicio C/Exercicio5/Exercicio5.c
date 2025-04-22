d4743a8c6938d91d5428d7c165c22cec625c0ac1
Tiago Velosa <ttvelosa2008@gmail.com>
2025-04-22 05:23:54 -0700

    update7

--- UFCD10789/Exercicio C/Exercicio5/Exercicio5.c
+++ UFCD10789/Exercicio C/Exercicio5/Exercicio5.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
  
 int main() {
-    int Num1, Num2, Num3;
-<<<<<<< HEAD
- 
-    // Leitura dos três valores inteiros
-=======
 
->>>>>>> 7623ced77532be6ed6b0d693739bd4036f33ed89
+    int Num1, Num2, Num3;
     printf("Insira o primeiro valor: ");
     scanf("%d", &Num1);
     printf("Insira o segundo valor: ");
@@ -36,8 +31,5 @@ int main() {
     printf("Ordem decrescente: %d, %d, %d\n", Num3, Num2, Num1);
  
     return 0;
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 7623ced77532be6ed6b0d693739bd4036f33ed89
+
---
+++ UFCD10789/Exercicio C/Exercicio7/Exercicio7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 
+int main() {
+
+    float nota1, nota2, nota3, media_final;
+ 
+    printf("Introduza a nota da 1ª prova (0 a 10): ");
+
+    scanf("%f", &nota1);
+ 
+    printf("Introduza a nota da 2ª prova (0 a 10): ");
+
+    scanf("%f", &nota2);
+ 
+    printf("Introduza a nota da 3ª prova (0 a 10): ");
+
+    scanf("%f", &nota3);
+
+    media_final = (nota1 * 2 + nota2 * 3 + nota3 * 5) / 10;
+
+    printf("\nMédia final: %.2f\n", media_final);
+ 
+    if (media_final >= 6.0) {
+
+        printf("Resultado: APROVADO\n");
+
+    } else {
+
+        printf("Resultado: REPROVADO\n");
+
+    }
+ 
+    return 0;
+}
+ 
