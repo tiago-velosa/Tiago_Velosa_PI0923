a2cc86479a203b8359ab87544f2366827490236b
tiago-velosa <ttvelosa2008@gmail.com>
2025-04-23 09:33:38 +0100

    Exercicio2.c

--- UFCD10789/Exercicio C/Exercicio2/Exercicio2.c
+++ UFCD10789/Exercicio C/Exercicio2/Exercicio2.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 
 void analisar_valores(int num1, int num2, int num3) {
-    int maior = num1, menor = num1;
+    int maior;
+    int menor;
 
-    if (num2 > maior) maior = num2;
-    if (num3 > maior) maior = num3;
-    if (num2 < menor) menor = num2;
-    if (num3 < menor) menor = num3;
+    maior = num1;
+    menor = num1;
+
+    if (num2 > maior) {
+        maior = num2;
+    }
+    if (num3 > maior) {
+        maior = num3;
+    }
+
+    if (num2 < menor) {
+        menor = num2;
+    }
+    if (num3 < menor) {
+        menor = num3;
+    }
 
     printf("Maior valor: %d\n", maior);
     printf("Menor valor: %d\n", menor);
 }
 
 int main() {
-    int num1, num2, num3;
-    printf("insira tres numeros inteiros: ");
+    int num1;
+    int num2;
+    int num3;
+
+    printf("Insira três números inteiros: ");
     scanf("%d %d %d", &num1, &num2, &num3);
+
     analisar_valores(num1, num2, num3);
+
     return 0;
 }
