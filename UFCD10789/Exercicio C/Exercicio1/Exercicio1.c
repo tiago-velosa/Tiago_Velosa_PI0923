53285b8c5977ff4ea2f718f631450ce9bf99ae04
tiago-velosa <ttvelosa2008@gmail.com>
2025-04-23 09:30:47 +0100

    Exercicio1.c

--- UFCD10789/Exercicio C/Exercicio1/Exercicio1.c
+++ UFCD10789/Exercicio C/Exercicio1/Exercicio1.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
-void converter_tempo(int segundos) {
-    int horas = segundos / 3600;
-    int segundos_restantes = segundos % 3600;
-    int minutos = segundos_restantes / 60;
-    int segundos_finais = segundos_restantes % 60;
-
-    printf("%d horas, %d minutos e %d segundos\n", horas, minutos, segundos_finais);
-}
-
-int main() {
+void converter_tempo(int total_segundos) {
+    int horas;
+    int resto_apos_horas;
+    int minutos;
     int segundos;
+
+    horas = total_segundos / 3600;
+    resto_apos_horas = total_segundos % 3600;
+    minutos = resto_apos_horas / 60;
+    segundos = resto_apos_horas % 60;
+
+    printf("%d horas, %d minutos e %d segundos\n", horas, minutos, segundos);
+}
+int main() {
+    int entrada_segundos;
+
     printf("Insira o tempo em segundos: ");
-    scanf("%d", &segundos);
-    converter_tempo(segundos);
+    scanf("%d", &entrada_segundos);
+
+    converter_tempo(entrada_segundos);
     return 0;
 }
