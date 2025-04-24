7b6c94d2fdbf508c03d7be57cdacfd626b08a0c8
Tiago Velosa <ttvelosa2008@gmail.com>
2025-04-24 00:26:03 -0700

    update ex1 python

---
+++ UFCD10789/Exercicio Python/Exercicio1.py
@@ -0,0 +1,7 @@
+print("Os 30 primeiros números ímpares são:")
+for i in range(1, 60, 2):  
+    print(i, end=" ")
+ 
+print("\nOs 30 primeiros números pares são:")
+for i in range(2, 61, 2):  
+    print(i, end=" ")
